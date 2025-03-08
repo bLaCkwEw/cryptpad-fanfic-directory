aa0ea4fe090a6b95cb28eea6e5160996f0346059
bLaCkwEw <35146970+bLaCkwEw@users.noreply.github.com>
2025-03-08 17:41:05 +0200

    initial commit
    not yet functional

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel';
+import auth from 'auth-astro';
+import db from '@astrojs/db';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	integrations: [auth(), db()],
+	output: "server",
+	adapter: vercel()
+});
